9c5c2f4bf0093822103cc0a6c9c31762fd2c78e1
Anton Smirnov <anton.smn@protonmail.com>
2021-09-29 14:16:26 +0300

    Add precompilation

--- src/RecoverPose.jl
+++ src/RecoverPose.jl
@@ -22,4 +22,24 @@ include("five_point/five_point.jl")
 
 include("pnp/p3p.jl")
 
+precompile(iterative_triangulation, (
+    SVector{2, Float64}, SVector{2, Float64},
+    SMatrix{3, 4, Float64, 12}, SMatrix{3, 4, Float64, 12}))
+precompile(iterative_triangulation, (
+    SVector{2, Float64}, SVector{2, Float64},
+    SMatrix{4, 4, Float64, 16}, SMatrix{4, 4, Float64, 16}))
+
+precompile(five_point_candidates, (
+    Vector{SVector{2, Float64}}, Vector{SVector{2, Float64}}))
+precompile(select_candidates, (
+    Vector{SMatrix{3, 3, Float64, 9}},
+    Vector{SVector{2, Float64}}, Vector{SVector{2, Float64}},
+    SMatrix{3, 3, Float64, 9}, SMatrix{3, 3, Float64, 9}))
+precompile(five_point, (
+    Vector{SVector{2, Float64}}, Vector{SVector{2, Float64}},
+    SMatrix{3, 3, Float64, 9}, SMatrix{3, 3, Float64, 9}))
+precompile(five_point_ransac, (
+    Vector{SVector{2, Float64}}, Vector{SVector{2, Float64}},
+    SMatrix{3, 3, Float64, 9}, SMatrix{3, 3, Float64, 9}))
+
 end
