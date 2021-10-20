2bfb6d7b355d8a8888ed9eee4f23bbab4d3a7c08
Anton Smirnov <anton.smn@protonmail.com>
2021-10-20 23:12:24 +0300

    remove local stuff

--- src/RecoverPose.jl
+++ src/RecoverPose.jl
@@ -48,9 +48,6 @@ end
         cache.work, lwork, info, 1, 1)
 end
 
-using Rotations
-using BenchmarkTools
-
 const PPolynomial = Polynomials.Polynomial
 
 include("ransac.jl")
@@ -58,78 +55,5 @@ include("five_point/utils.jl")
 include("five_point/chirality.jl")
 include("five_point/five_point.jl")
 include("pnp/p3p.jl")
-
-function main()
-    res = 1024
-    pp = res ÷ 2
-    K = SMatrix{3, 3, Float64}(
-        910, 0, 0,
-        0, 910, 0,
-        pp, pp, 1,
-    )
-    K_inv = inv(K)
-
-    θ = π / 8
-    R = RotXYZ(rand() * θ, rand() * θ, rand() * θ)
-    t = SVector{3, Float64}(rand(), rand() * 2, rand() * 3)
-
-    P_target = RecoverPose.get_transformation(R, t)
-    P2 = K * P_target
-
-    x1, x2 = SVector{2, Float64}[], SVector{2, Float64}[]
-
-    n_points = 1000
-    for i in 1:n_points
-        p1 = SVector{3, Float64}(rand() * res, rand() * res, 1.0)
-        p1h = SVector{4, Float64}((K_inv * p1)..., 1.0)
-        p2 = P2 * p1h
-        p2 = p2[1:2] .* (1 / p2[3])
-
-        push!(x1, p1[[1, 2]])
-        push!(x2, p2[[1, 2]])
-    end
-
-    x1, x2, K, K
-end
-
-function tt()
-    T = Float64
-    P1 = SMatrix{3, 4, T}(
-        0.999701, -0.0171452, 0.0174497,
-        0.0174497, 0.999695, -0.0174524,
-        -0.017145, 0.0177517, 0.999695,
-        -500, -100, -100,
-    )
-    P2 = SMatrix{3, 4, T}(
-        0.99969, 0.0177543, -0.0174497,
-        -0.0174497, 0.999695, 0.0174524,
-        0.0177543, -0.0171425, 0.999695,
-        500, -100, -100,
-    )
-    K = SMatrix{3, 3, T}(
-        7291.67, 0, 0,
-        0, 7291.67, 0,
-        639.5, 511.5, 1,
-    )
-
-    P1 = K * P1
-    P2 = K * P2
-
-    p1 = SVector{2, T}(146, 642.288)
-    p2 = SVector{2, T}(1137.31, 385.201)
-
-    cache = GEEV4x4Cache()
-    t = triangulate(p1, p2, P1, P2, cache)
-    @show t / t[4]
-    t = triangulate(p1, p2, P1, P2)
-    @show t / t[4]
-
-    @btime triangulate($p1, $p2, $P1, $P2, $cache)
-    @btime triangulate($p1, $p2, $P1, $P2)
-    @btime for _ in 1:1000_000 triangulate($p1, $p2, $P1, $P2, $cache) end
-    @btime for _ in 1:1000_000 triangulate($p1, $p2, $P1, $P2) end
-
-    nothing
-end
 
 end
